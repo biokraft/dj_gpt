3f073179d4d309d07050e4779fd2e75a7bc8b197
Seán Baufeld <sean.baufeld@check24.de>
2024-04-21 23:35:16 +0200

    Adjust HTML theme options and set static path in Sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -26,9 +26,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-
+html_static_path = ['_static']
+html_logo = "logo.png"
 html_theme_options = {
-    "logo_only": False,
+    'logo_only': True,
+    'display_version': False,
 }
-
-html_logo = "_static/logo.png"
